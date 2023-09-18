17ef9b2a2068d3c10da2323a934355eee802fa56
dondycles <johnrod.dondoyano@gmail.com>
2023-09-18 17:04:59 +0800

    modified:   app/(pages)/web-novel/page.tsx

--- app/(pages)/web-novel/page.tsx
+++ app/(pages)/web-novel/page.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
+import Component from "./Component";
 export const metadata: Metadata = {
   title: "HeroeZ | Web Novel",
   description: "Heroes in 6th Dimension.",
 };
 
-export default function WebNovel({ children }: { children: React.ReactNode }) {
+export default function WebNovel() {
   return (
     <>
       <h1>
         WEB <span className="text-primary">NOVEL</span>
       </h1>
-      {children}
+      <Component />
     </>
   );
 }
