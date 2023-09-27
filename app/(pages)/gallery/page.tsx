aeb8dc02b212718c6f48d5fcf36182932a236c58
dondycles <johnrod.dondoyano@gmail.com>
2023-09-27 23:27:49 +0800

    modified:   app/(pages)/gallery/page.tsx

--- app/(pages)/gallery/page.tsx
+++ app/(pages)/gallery/page.tsx
@@ -7,29 +7,35 @@ import { MdOutlineKeyboardArrowUp } from "react-icons/md";
 
 import { useState } from "react";
 import GlowingBorder from "@/app/components/Styles/GlowingBorder";
-import { Button } from "@nextui-org/react";
+import { Button, Divider } from "@nextui-org/react";
+import nft1 from "@/public/images/nfts/1.webp";
+import nft2 from "@/public/images/nfts/2.webp";
+import nft3 from "@/public/images/nfts/3.webp";
+import nft4 from "@/public/images/nfts/4.webp";
+import nft5 from "@/public/images/nfts/5.webp";
+import nft6 from "@/public/images/nfts/6.webp";
 
 export default function Page() {
-  const [nfts, setNfts] = useState([
+  const nfts = [
     {
-      src: `/images/nfts/1.webp`,
+      src: nft1,
     },
     {
-      src: `/images/nfts/2.webp`,
+      src: nft2,
     },
     {
-      src: `/images/nfts/3.webp`,
+      src: nft3,
     },
     {
-      src: `/images/nfts/4.webp`,
+      src: nft4,
     },
     {
-      src: `/images/nfts/5.webp`,
+      src: nft5,
     },
     {
-      src: `/images/nfts/6.webp`,
+      src: nft6,
     },
-  ]);
+  ];
 
   return (
     <Scrollable>
@@ -38,7 +44,7 @@ export default function Page() {
         fuga. Qui!
       </Paragraph>
       <div className=" flex flex-row gap-4 max-h-[300px] sm:max-h-[400px] md:max-h-[500px] h-full overflow-hidden group container p-4">
-        <div className=" flex flex-row gap-4 con-partition ">
+        <div className=" flex flex-row gap-4  ">
           {nfts.map((nft, i) => {
             const [showDetails, setShowDetails] = useState(false);
             return (
@@ -46,104 +52,53 @@ export default function Page() {
                 key={i * i}
                 className=" h-full w-auto aspect-square glowing-border p-1 bg-primary/20 rounded-xl relative overflow-hidden"
               >
-                <img
+                <Image
                   className="h-full w-auto aspect-square object-cover rounded-xl duration-100 "
                   src={nft.src}
                   alt={String(nft.src)}
+                  placeholder="blur"
                 />
-                <div className="absolute top-4 left-4 rounded-md bg-background/50 px-4 flex items-center justify-center">
-                  <p className="text-sizing text-white drop-shadow-[0_1px_1px_#00000088] font-montserrat">
-                    Hero Name
-                  </p>
-                </div>
+
                 <div
                   className={`
-                  absolute bottom-1 h-1/2 left-1 right-1 rounded-b-xl flex duration-300 z-10 flex-col bg-background/50 cursor-pointer ${
-                    showDetails
-                      ? "translate-y-[0%]"
-                      : "translate-y-[calc(100%-36px)]"
+                  absolute bottom-4 h-1/2 left-4 right-4 rounded-xl text-white flex duration-300 z-10 flex-col bg-black/50 cursor-pointer overflow-hidden ${
+                    !showDetails ? "max-h-[40px]" : "max-h-[400px]"
                   }`}
                 >
                   <Button
                     onClick={() => setShowDetails((prev) => !prev)}
-                    className="bg-transparent min-h-[40px]"
+                    className="bg-transparent min-h-[40px] p-0"
                   >
-                    <MdOutlineKeyboardArrowUp />
+                    <p className="text-sizing text-white drop-shadow-[0_1px_1px_#00000088] font-montserrat flex flex-row items-center justify-between w-full p-4">
+                      Hero Name
+                      <span
+                        className={`${
+                          showDetails ? "rotate-180" : "rotate-0"
+                        } duration-500 text-white text-2xl`}
+                      >
+                        <MdOutlineKeyboardArrowUp />
+                      </span>
+                    </p>
                   </Button>
-                  <div className="max-h-full overflow-auto p-4">
-                    <p>
-                      Skills: ipsum dolor sit amet consectetur adipisicing elit.
-                      Tempora
-                    </p>
-                    <br />
-                    <p>
-                      Skills: ipsum dolor sit amet consectetur adipisicing elit.
-                      Tempora
-                    </p>
-                    <br />
+                  <Divider />
+                  <div className="py-4 overflow-hidden ">
+                    <div className="max-h-full overflow-auto rounded-xl px-4 ">
+                      <p>
+                        Skills: ipsum dolor sit amet consectetur adipisicing
+                        elit. Tempora
+                      </p>
+                      <br />
+                      <p>
+                        Skills: ipsum dolor sit amet consectetur adipisicing
+                        elit. Tempora
+                      </p>
+                      <br />
 
-                    <p>
-                      Skills: ipsum dolor sit amet consectetur adipisicing elit.
-                      Tempora
-                    </p>
-                  </div>
-                </div>
-
-                <div className="absolute top-0 bottom-0 left-0 right-0">
-                  <GlowingBorder />
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className=" flex flex-row gap-4 con-partition ">
-          {nfts.map((nft, i) => {
-            const [showDetails, setShowDetails] = useState(false);
-            return (
-              <div
-                key={i}
-                className=" h-full w-auto aspect-square glowing-border p-1 bg-primary/20 rounded-xl relative overflow-hidden"
-              >
-                <img
-                  className="h-full w-auto aspect-square object-cover rounded-xl duration-100 "
-                  src={nft.src}
-                  alt={String(nft.src)}
-                />
-                <div className="absolute top-4 left-4 rounded-md bg-background/50 px-4 flex items-center justify-center">
-                  <p className="text-sizing text-white drop-shadow-[0_1px_1px_#00000088] font-montserrat">
-                    Hero Name
-                  </p>
-                </div>
-                <div
-                  className={`
-                  absolute bottom-1 h-1/2 left-1 right-1 rounded-b-xl flex duration-300 z-10 flex-col bg-background/50 cursor-pointer ${
-                    showDetails
-                      ? "translate-y-[0%]"
-                      : "translate-y-[calc(100%-36px)]"
-                  }`}
-                >
-                  <Button
-                    onClick={() => setShowDetails((prev) => !prev)}
-                    className="bg-transparent min-h-[40px]"
-                  >
-                    <MdOutlineKeyboardArrowUp />
-                  </Button>
-                  <div className="max-h-full overflow-auto p-4">
-                    <p>
-                      Skills: ipsum dolor sit amet consectetur adipisicing elit.
-                      Tempora
-                    </p>
-                    <br />
-                    <p>
-                      Skills: ipsum dolor sit amet consectetur adipisicing elit.
-                      Tempora
-                    </p>
-                    <br />
-
-                    <p>
-                      Skills: ipsum dolor sit amet consectetur adipisicing elit.
-                      Tempora
-                    </p>
+                      <p>
+                        Skills: ipsum dolor sit amet consectetur adipisicing
+                        elit. Tempora
+                      </p>
+                    </div>
                   </div>
                 </div>
 
