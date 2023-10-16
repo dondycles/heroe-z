239d59fdf98311f7a8216007d2b313953933873c
dondycles <johnrod.dondoyano@gmail.com>
2023-10-17 00:49:10 +0800

    modified:   app/(pages)/gallery/page.tsx 	modified:   app/components/ui/HeroCard.tsx 	deleted:    public/images/nfts/1.png 	new file:   public/images/nfts/1.webp 	modified:   public/images/nfts/10.webp 	modified:   public/images/nfts/12.webp 	modified:   public/images/nfts/13.webp 	modified:   public/images/nfts/14.webp 	modified:   public/images/nfts/16.webp 	modified:   public/images/nfts/17.webp 	modified:   public/images/nfts/18.webp 	modified:   public/images/nfts/2.webp 	modified:   public/images/nfts/3.webp 	modified:   public/images/nfts/4.webp 	deleted:    public/images/nfts/5.jpg 	new file:   public/images/nfts/5.webp 	modified:   public/images/nfts/6.webp 	modified:   public/images/nfts/7.webp

--- app/components/ui/HeroCard.tsx
+++ app/components/ui/HeroCard.tsx
@@ -18,47 +18,6 @@ export default function HeroCard({ nft }: { nft: any[any] }) {
         placeholder="blur"
       />
 
-      {/* <div
-        onClick={(e) => e.stopPropagation()}
-        className={`
-    absolute bottom-2 h-1/2 left-2 right-2 rounded-xl text-white flex duration-300 z-10 flex-col bg-black/20 backdrop-blur-[2px] backdrop-brightness-75 cursor-pointer overflow-hidden ${
-      !showDetails ? "max-h-[30px]" : "max-h-[400px]"
-    }`}
-      >
-        <Button
-          onClick={() => setShowDetails((prev) => !prev)}
-          className="bg-transparent min-h-[30px] p-2"
-        >
-          <p className="text-sizing text-white drop-shadow-[0_1px_1px_#00000088] font-montserrat w-full text-center">
-            Hero Name
-            <span
-              className={`${
-                showDetails ? "rotate-180" : "rotate-0"
-              } duration-500 text-white text-2xl`}
-            >
-              <MdOutlineKeyboardArrowUp />
-            </span>
-          </p>
-        </Button>
-        <Divider />
-        <div className="py-2 overflow-hidden ">
-          <div className="max-h-full overflow-auto rounded-xl px-2 ">
-            <p>
-              Skills: ipsum dolor sit amet consectetur adipisicing elit. Tempora
-            </p>
-            <br />
-            <p>
-              Skills: ipsum dolor sit amet consectetur adipisicing elit. Tempora
-            </p>
-            <br />
-
-            <p>
-              Skills: ipsum dolor sit amet consectetur adipisicing elit. Tempora
-            </p>
-          </div>
-        </div>
-      </div> */}
-
       <div className="absolute top-0 bottom-0 left-0 right-0">
         <GlowingBorder />
       </div>
