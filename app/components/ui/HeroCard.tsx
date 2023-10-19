02f64cdc367ad55a90ffcba58e4f739f5b3ce91b
dondycles <johnrod.dondoyano@gmail.com>
2023-10-19 22:38:15 +0800

    modified:   app/components/ui/HeroCard.tsx

--- app/components/ui/HeroCard.tsx
+++ app/components/ui/HeroCard.tsx
@@ -1,17 +1,25 @@
 import GlowingBorder from "@/app/components/Styles/GlowingBorder";
 import { Button, Divider } from "@nextui-org/react";
 import Image from "next/image";
-import { Key, useState } from "react";
+import { Key, useEffect, useRef, useState } from "react";
 
 export default function HeroCard({ nft }: { nft: any[any] }) {
   const [showDetails, setShowDetails] = useState(false);
+  const [width, setWidth] = useState<number | undefined>(0);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    setWidth(ref.current?.clientHeight);
+  }, []);
   return (
     <div
+      ref={ref}
       onClick={() => setShowDetails(false)}
-      className=" h-full w-auto aspect-square glowing-border p-[2px] bg-primary/20 rounded-md sm:rounded-xl relative overflow-hidden"
+      style={{ width: width }}
+      className=" h-full aspect-square glowing-border p-[2px] bg-primary/20 rounded-md sm:rounded-xl relative overflow-hidden"
     >
       <Image
-        className="h-full w-auto aspect-square object-cover rounded-md sm:rounded-xl duration-100 "
+        className="h-full aspect-square object-cover rounded-md sm:rounded-xl duration-100 "
         src={nft.src}
         quality={100}
         alt={String(nft.src)}
