c7ecd73cff8f917aa940608da1dfefbc0976a908
dondycles <johnrod.dondoyano@gmail.com>
2023-10-19 22:42:53 +0800

    modified:   app/components/ui/HeroCard.tsx

--- app/components/ui/HeroCard.tsx
+++ app/components/ui/HeroCard.tsx
@@ -15,11 +15,11 @@ export default function HeroCard({ nft }: { nft: any[any] }) {
     <div
       ref={ref}
       onClick={() => setShowDetails(false)}
-      style={{ width: width }}
-      className=" h-full aspect-square glowing-border p-[2px] bg-primary/20 rounded-md sm:rounded-xl relative overflow-hidden"
+      style={{ minWidth: width }}
+      className=" h-full w-auto aspect-square glowing-border p-[2px] bg-primary/20 rounded-md sm:rounded-xl relative overflow-hidden"
     >
       <Image
-        className="h-full aspect-square object-cover rounded-md sm:rounded-xl duration-100 "
+        className="h-full w-auto aspect-square object-cover rounded-md sm:rounded-xl duration-100 "
         src={nft.src}
         quality={100}
         alt={String(nft.src)}
