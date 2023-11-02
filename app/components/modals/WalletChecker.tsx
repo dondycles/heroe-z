1ebb84fe197bb192033404d33859c19f79c10720
dondycles <johnrod.dondoyano@gmail.com>
2023-11-02 11:57:54 +0800

    modified:   app/components/modals/WalletChecker.tsx 	modified:   package.json 	modified:   pnpm-lock.yaml 	new file:   public/Wallets/Guarranted.json 	new file:   public/Wallets/OG.json 	new file:   public/Wallets/Whitelist.json

--- app/components/modals/WalletChecker.tsx
+++ app/components/modals/WalletChecker.tsx
@@ -1,26 +1,144 @@
-import { useAnimateStore, useNavigationStore, useThemeStore } from "@/store";
+import { useNavigationStore, useThemeStore } from "@/store";
 import {
-  Button,
   Divider,
   Input,
   Modal,
   ModalBody,
   ModalContent,
-  ModalFooter,
   ModalHeader,
 } from "@nextui-org/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import GlowingBorder from "../Styles/GlowingBorder";
-import { usePathname } from "next/navigation";
-import { MdWarning } from "react-icons/md";
-import Link from "next/link";
-
+import { MdDangerous } from "react-icons/md";
+import useSWR from "swr";
+import { IoIosWarning } from "react-icons/io";
+import { FaCheckCircle } from "react-icons/fa";
 export default function WalletChecker() {
-  const animateState = useAnimateStore();
-  const [isOpen, setIsOpen] = useState(true);
   const theme = useThemeStore();
-  const pathname = usePathname();
   const navigation = useNavigationStore();
+
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const [message, setMessage] = useState(
+    <span>Enter yout wallet address to check your eligiblity to summon.</span>
+  );
+
+  const { data: whitelistData, error: whitelistError } = useSWR(
+    "/Wallets/whitelist.json",
+    fetcher
+  );
+  const { data: ogData, error: ogError } = useSWR("/Wallets/OG.json", fetcher);
+  const { data: gaData, error: gaError } = useSWR(
+    "/Wallets/Guarranted.json",
+    fetcher
+  );
+
+  const handleChecks = (addr: string) => {
+    if (addr === "") {
+      setMessage(
+        <span>
+          Enter yout wallet address to check your eligiblity to summon.
+        </span>
+      );
+      return;
+    }
+
+    let isOG = false;
+    let isWhitelist = false;
+    let isGuaranteed = false;
+    let isValidAddress = true;
+
+    // Check if the address is valid
+    if (String(addr).length !== 42) {
+      isValidAddress = false;
+      setMessage(
+        <p className="text-warning flex flex-row items-center justify-center gap-2">
+          <span>Not a valid address</span>
+          <span className="text-2xl">
+            <IoIosWarning />
+          </span>
+        </p>
+      );
+    }
+    if (isValidAddress) {
+      // Only check OG, Whitelist, and Guaranteed if the address is valid
+      if (ogData) {
+        const loweracc = ogData.map((acc: string) => acc.toLowerCase());
+        const useradd = addr.toLowerCase();
+
+        if (loweracc.includes(useradd)) {
+          isOG = true;
+        }
+      }
+
+      if (whitelistData) {
+        const loweracc = whitelistData.map((acc: string) => acc.toLowerCase());
+        const useradd = addr.toLowerCase();
+
+        if (loweracc.includes(useradd)) {
+          isWhitelist = true;
+        }
+      }
+
+      if (gaData) {
+        const loweracc = gaData.map((acc: string) => acc.toLowerCase());
+        const useradd = addr.toLowerCase();
+
+        if (loweracc.includes(useradd)) {
+          isGuaranteed = true;
+        }
+      }
+
+      // Update the message based on the results
+      if (!isOG && !isWhitelist && !isGuaranteed) {
+        setMessage(
+          <p className="text-danger flex flex-row items-center justify-center gap-2">
+            <span>Sorry! you are not qualified to summon a Hero.</span>
+            <span className="text-2xl">
+              <MdDangerous />
+            </span>
+          </p>
+        );
+      } else if (isOG) {
+        setMessage(
+          <p className="text-success flex flex-row items-center justify-center gap-2">
+            <span>Congratulations! You are listed on OG mint.</span>
+            <span className="text-2xl">
+              <FaCheckCircle />
+            </span>
+          </p>
+        );
+      } else if (isWhitelist) {
+        setMessage(
+          <p className="text-success flex flex-row items-center justify-center gap-2">
+            <span>Congratulations! You are listed on Whitelist mint.</span>
+            <span className="text-2xl">
+              <FaCheckCircle />
+            </span>
+          </p>
+        );
+      } else if (isGuaranteed) {
+        setMessage(
+          <p className="text-success flex flex-row items-center justify-center gap-2">
+            <span>Congratulations! You are listed on Guaranteed mint.</span>
+            <span className="text-2xl">
+              <FaCheckCircle />
+            </span>
+          </p>
+        );
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (!navigation.openWalletChecker) {
+      setMessage(
+        <span>
+          Enter yout wallet address to check your eligiblity to summon.
+        </span>
+      );
+    }
+  }, [navigation.openWalletChecker]);
+
   return (
     <Modal
       placement="center"
@@ -33,32 +151,27 @@ export default function WalletChecker() {
       className={`${theme.mode} bg-background text-foreground `}
       closeButton={false}
     >
-      <ModalContent className="overflow-hidden relative glowing-border p-[2px]">
+      <ModalContent className="overflow-hidden relative glowing-border">
         <ModalHeader className="flex flex-col gap-1 font-montserrat text-center text-2xl bg-background/50 rounded-t-xl text-primary">
           Wallet Checker
         </ModalHeader>
         <Divider />
-        <ModalBody className="bg-background/50 ">
-          <p>Enter yout wallet address to check your eligiblity to summon.</p>
+        <ModalBody className="bg-background/50 py-4">
           <Input
-            placeholder="Wallet Address"
             className="max-w-[500px] mx-auto"
-          />
-        </ModalBody>
-        <Divider />
-        <ModalFooter className="bg-background/50 rounded-b-xl ">
-          <Button
+            type={"text"}
+            variant="bordered"
             color="primary"
-            variant="shadow"
-            onPress={() => {}}
-            className="glowing-border p-[2px] relative bg-primary/10 after:bg-background/90 sm:after:hover:bg-background/50 "
-          >
-            <p className="text-xs font-montserrat p-3 text-primary">
-              CHECK WALLET
-            </p>
-            <GlowingBorder />
-          </Button>
-          {/* <Button
+            placeholder={"Wallet address"}
+            onChange={(e) => {
+              handleChecks(e.target.value);
+            }}
+          />
+          <div className="text-center">{message}</div>
+        </ModalBody>
+        {/* <Divider /> */}
+        {/* <ModalFooter className="bg-background/50 rounded-b-xl ">
+          <Button
             color="primary"
             variant="shadow"
             onPress={() => {
@@ -71,8 +184,8 @@ export default function WalletChecker() {
               GO BACK TO HOME
             </p>
             <GlowingBorder />
-          </Button> */}
-        </ModalFooter>
+          </Button>
+        </ModalFooter> */}
         <GlowingBorder />
       </ModalContent>
     </Modal>
